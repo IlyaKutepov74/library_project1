cd648a3495578150fe349b252de391826b44fd59
9santa <wotegorb@mail.ru>
2026-05-02 19:54:42 +0300

    sqlalchemy base & connection to postgres

---
+++ app/db/session.py
@@ -0,0 +1,20 @@
+import os
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/library_db"
+)
+
+engine = create_async_engine(
+    DATABASE_URL,
+)
+
+async_session_maker = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_session():
+    async with async_session_maker() as session:
+        yield session
